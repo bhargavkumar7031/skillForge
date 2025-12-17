cbd9f6e4e18301f5cbb881798f20926cefab273c
UTTHUNGADC\bhargav.b <bhargav.b@utthunga.com>
2025-12-17 14:36:07 +0530

    service and validator seperated

--- src/main/java/org/skillforge/service/UserService.java
+++ src/main/java/org/skillforge/service/UserService.java
@@ -4,6 +4,7 @@ import org.skillforge.domain.User;
 import org.skillforge.dto.signupRequestDTO;
 import org.skillforge.exceptions.InvalidInputException;
 import org.skillforge.repository.UserRepository;
+import org.skillforge.validators.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,27 +17,13 @@ public class UserService {
     @Autowired
     private UserRepository userRepo;
 
+    @Autowired
+    private UserValidator userValidator;
+
 
     public void add(signupRequestDTO signupRequest) {
-        String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
-        String PASS_REGEX = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&-])[A-Za-z\\d@$!%*?&-]{8,}$";
-
-
         User user = new User();
-
-        if(!(Pattern.matches(EMAIL_REGEX, signupRequest.getEmail()))) {
-            System.out.println(signupRequest.getEmail());
-            throw new InvalidInputException("Invalid Email");
-        }
-
-        if(signupRequest.getPassword() == "") {
-            throw new InvalidInputException("Password cannot be empty");
-        }
-
-        if(!(Pattern.matches(PASS_REGEX, signupRequest.getPassword()))) {
-            throw new InvalidInputException("Password should be atleast 8 characters long, consisting of alphabets, numbers and special characters");
-        }
-
+        userValidator.userValidation(signupRequest);
         user.setEmail(signupRequest.getEmail());
         user.setPassword(signupRequest.getPassword());
         user.setCreatedAt(new Date());
---
+++ src/main/java/org/skillforge/validators/UserValidator.java
@@ -0,0 +1,28 @@
+package org.skillforge.validators;
+
+import org.skillforge.dto.signupRequestDTO;
+import org.skillforge.exceptions.InvalidInputException;
+import org.springframework.stereotype.Component;
+
+import java.util.regex.Pattern;
+
+@Component
+public class UserValidator {
+    public void userValidation(signupRequestDTO request) {
+        String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+        String PASS_REGEX = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&-])[A-Za-z\\d@$!%*?&-]{8,}$";
+
+        if (!(Pattern.matches(EMAIL_REGEX, request.getEmail()))) {
+            System.out.println(request.getEmail());
+            throw new InvalidInputException("Invalid Email");
+        }
+
+        if (request.getPassword() == "") {
+            throw new InvalidInputException("Password cannot be empty");
+        }
+
+        if (!(Pattern.matches(PASS_REGEX, request.getPassword()))) {
+            throw new InvalidInputException("Password should be atleast 8 characters long, consisting of alphabets, numbers and special characters");
+        }
+    }
+}
